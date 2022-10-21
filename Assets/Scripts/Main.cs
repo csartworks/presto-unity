ca61dae72b037cfc1e6c118ed95bd0ef2c4b8447
rpopic2 <rpopic2@gmail.com>
2022-10-22 04:27:31 +0900

    change project settings

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -7,13 +7,14 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
     public static Dictionary<string, GlyphName> GlyphNames { get; private set; } = new();
+    [SerializeField] private TMP_Text _score;
     private void Awake()
     {
         var json = Resources.Load<TextAsset>("raw/glyphnames");
         GlyphNames = JsonParser.Parse(json.text);
 
         char gclef = GlyphNames["gClef"].Codepoint;
-        GameObject.FindObjectOfType<TMP_Text>().text = gclef.ToString();
+        _score.text = gclef.ToString();
     }
 }
 
